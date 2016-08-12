e18e030626067efc0e2687d952f8c5344b45d41b
kerrongordon <kgpsounds.com@gmail.com>
2016-08-12 19:01:42 -0400

    update

--- source/_js/main.js
+++ source/_js/main.js
@@ -1,1 +1,16 @@
-console.log('gordon');
+(function(){
+  "use strict";
+
+  let notification = () => {
+    $('.notification__close').click(function() {
+      $('.notification').removeClass('notification__open');
+      console.log('click');
+    });
+
+    $('.test').addClass('notification__open');
+    console.log('kgp');
+  };
+
+  notification();
+
+})();
