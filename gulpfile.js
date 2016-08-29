2a03cb6a6248f501628fc40e6d812a9cc4a71b5e
kerrongordon <kgpsounds.com@gmail.com>
2016-08-29 15:32:23 -0400

    added jade and fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,198 +1,183 @@
-'use strict';
+'use strict'
 
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const jekyll = require('gulp-jekyll');
-const sass   = require('gulp-sass');
-const uglify = require('gulp-uglify');
-const pump = require('pump');
-const del = require('del');
-const cleanCSS = require('gulp-clean-css');
-const concat = require('gulp-concat');
-const plumber = require('gulp-plumber');
-const sourcemaps = require('gulp-sourcemaps');
-const notify = require("gulp-notify");
-const htmlminify = require("gulp-html-minify");
-const autoprefixer = require('gulp-autoprefixer');
-const minifyCSS = require('gulp-minify-css');
-const ghPages = require('gulp-gh-pages');
-const imagemin = require('gulp-imagemin');
-const gulpWebpack = require('gulp-webpack');
-const webpack = require('webpack');
-const browserSync = require('browser-sync').create();
-
+const gulp = require('gulp')
+const jade = require('gulp-jade')
+const babel = require('gulp-babel')
+const jekyll = require('gulp-jekyll')
+const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
+const pump = require('pump')
+const del = require('del')
+const cleanCSS = require('gulp-clean-css')
+const concat = require('gulp-concat')
+const plumber = require('gulp-plumber')
+const sourcemaps = require('gulp-sourcemaps')
+const notify = require('gulp-notify')
+const htmlminify = require('gulp-html-minify')
+const autoprefixer = require('gulp-autoprefixer')
+const minifyCSS = require('gulp-minify-css')
+const ghPages = require('gulp-gh-pages')
+const imagemin = require('gulp-imagemin')
+const gulpWebpack = require('gulp-webpack')
+const webpack = require('webpack')
+const browserSync = require('browser-sync').create()
+const runSequence = require('gulp-run-sequence')
 
 let source = {
-	js: 'source/_js/**/*.js',
-	sass: 'source/_sass/**/*.sass',
-	html: 'source/**/*.html',
-	img: 'source/assets/img/',
-	output: {
-		js: 'source/assets/js/',
-		css: 'source/assets/css/'
-	}
+  js: 'source/_js/**/*.js',
+  sass: 'source/_sass/**/*.sass',
+  html: 'source/**/*.html',
+  img: 'source/assets/img/',
+  jade: 'source/_jade/**/*.jade',
+  output: {
+    js: 'source/assets/js/',
+    css: 'source/assets/css/',
+    html: 'source/_includes/'
+  }
 }
 
 let live = {
-	css: '_live/assets/css/',
-	js: '_live/assets/js/',
-	img: '_live/assets/img/',
-	html: '_live/**/*.html',
-	all: '_live/*'
+  css: '_live/assets/css/',
+  js: '_live/assets/js/',
+  img: '_live/assets/img/',
+  html: '_live/**/*.html',
+  all: '_live/*'
 }
 
 let build = {
-	dest: 'build/',
-	js: 'build/assets/js/',
-	css: 'build/assets/css/',
-	img: 'build/assets/img/'
+  dest: 'build/',
+  js: 'build/assets/js/',
+  css: 'build/assets/css/',
+  img: 'build/assets/img/'
 }
 
-//==========================================================//
-//=====================Clean Build==========================//
-//==========================================================//
-
+//========== Remove Old Build ===========//
 gulp.task('clean:build', () => {
-	return del(build.dest)
-});
+  return del(build.dest)
+})
 
+//========== Remove Old Jade Build ==========//
+gulp.task('jade:clean', () => {
+  return del(source.output.html)
+})
 
-//==========================================================//
-//========================gulpWebpack============================//
-//==========================================================//
+//========== Build Jade =========//
+gulp.task('jade:build', () => {
+  return gulp.src(source.jade)
+  	.pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(jade({ pretty: true }))
+    .pipe(gulp.dest(source.output.html))
+})
 
-// gulp.task('concat', ['clean:build'], () => {
-//   return gulp.src(source.js)
-//   	.pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
-//     .pipe(sourcemaps.init())
-//     .pipe(concat('main.js'))
-//     .pipe(sourcemaps.write())
-//     .pipe(gulp.dest(source.output.js));
-// });
-gulp.task('gulpWebpack', ['clean:build'], () => {
+//========== Web Pack Build ==========//
+gulp.task('Webpack:build', () => {
   return gulp.src('source/_js/main.js')
-  	.pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
-    .pipe(gulpWebpack({ output: { filename: 'main.js', }, }))
-		.pipe(concat('main.js'))
-    .pipe(gulp.dest(source.output.js));
-});
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(gulpWebpack({ output: { filename: 'main.js' } }))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(source.output.js))
+})
 
-//==========================================================//
-//=========================SASS=============================//
-//==========================================================//
-
-gulp.task('sass', ['gulpWebpack'], () => {
-	return gulp.src(source.sass)
-		.pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
-		.pipe(sass({ indentedSyntax: true }))
-		.pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
+//========== Sass Build ==========//
+gulp.task('sass:build', () => {
+  return gulp.src(source.sass)
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(sass({ indentedSyntax: true }))
+    .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
     .pipe(gulp.dest(source.output.css))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+})
 
-//==========================================================//
-//=======================Jekyll=============================//
-//==========================================================//
+//========= Jekyll Build ==========//
+gulp.task('jekyll:build', () => {
+  return gulp.src(process.cwd())
+  	.pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(jekyll({
+      bundleExec: false,
+      quiet: true,
+      safe: true,
+      layouts: '_layouts',
+      plugins: '_plugins',
+      source: './source/',
+      destination: '_live'
+    }))
+})
 
-gulp.task('jekyll', ['sass'], () => {
-	return gulp.src(process.cwd())
-		.pipe(jekyll({
-			bundleExec: false,
-			quiet: true,
-			safe: true,
-			layouts: '_layouts',
-      		plugins: '_plugins',
-      		source: './source/',
-      		destination: '_live'
-		}))
-});
+//========== Html Build =========//
+gulp.task('html:build', () => {
+  return gulp.src(live.html)
+    .pipe(htmlminify())
+    .pipe(gulp.dest(build.dest))
+})
 
-//==========================================================//
-//=====================Html Build===========================//
-//==========================================================//
-
-gulp.task('html:build', ['jekyll'], () => {
-	return gulp.src(live.html)
-		.pipe(htmlminify())
-		.pipe(gulp.dest(build.dest));
-});
-
-//==========================================================//
-//=======================Js Build===========================//
-//==========================================================//
-
-// gulp.task('js:build', ['html:build'], (cb) => {
-//   pump([gulp.src('_live/assets/js/main.js'), uglify(), gulp.dest(build.js)], cb);
-// });
-
-gulp.task('js:build', ['html:build'], () => {
-	gulp.src(live.js + 'main.js')
+//========== js Build ==========//
+gulp.task('js:build', () => {
+  gulp.src(live.js + 'main.js')
     .pipe(gulpWebpack({
       output: {
-        filename: 'main.js',
+        filename: 'main.js'
       },
-      plugins: [new webpack.optimize.UglifyJsPlugin()],
+      plugins: [new webpack.optimize.UglifyJsPlugin()]
     }, webpack))
-    .pipe(gulp.dest(build.js));
-});
+    .pipe(gulp.dest(build.js))
+})
 
-//==========================================================//
-//======================Css Build===========================//
-//==========================================================//
+//========== css Build ==========//
+gulp.task('css:build', () => {
+  return gulp.src(live.css + '**/*.css')
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(build.css))
+})
 
-gulp.task('css:build', ['js:build'], () => {
-	return gulp.src(live.css + '**/*.css')
-		.pipe(minifyCSS())
-    	.pipe(gulp.dest(build.css));
-});
+//========= Img Build ==========//
+gulp.task('img:build', () => {
+  return gulp.src(source.img + '**/*')
+    .pipe(imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: false,
+      svgoPlugins: [{
+        removeViewBox: false
+      }]
+    }))
+    .pipe(gulp.dest(build.img))
+})
 
-//==========================================================//
-//========================Img Build=========================//
-//==========================================================//
+//========= Copy Build ==========//
+gulp.task('copy:build', () => {
+  return gulp.src(live.all + '.{txt,xml,ico,png}')
+    .pipe(gulp.dest(build.dest))
+})
 
-gulp.task('img:build', ['css:build'], () => {
-    return gulp.src(source.img + '**/*')
-        .pipe(imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: false,
-            svgoPlugins: [{
-                removeViewBox: false
-            }]
-        }))
-        .pipe(gulp.dest(build.img));
-});
+//========== Run Build ==========//
+gulp.task('build', (cb) => {
+  runSequence(
+    'clean:build',
+	'jade:clean',
+	'jade:build',
+    'Webpack:build',
+    'sass:build',
+    'jekyll:build',
+    'html:build',
+    'js:build',
+    'css:build',
+    'img:build',
+    'copy:build',
+    cb)
+})
 
-//==========================================================//
-//=========================Build============================//
-//==========================================================//
+//========== Run Server ==========//
+gulp.task('serve', ['jekyll:build'], () => {
+  browserSync.init({ server: './_live' })
+  gulp.watch(source.sass, ['sass:build', 'jekyll:build'])
+  gulp.watch(source.jade, ['jade:build'])
+  gulp.watch(source.html, ['jekyll:build'])
+  gulp.watch(source.js, ['Webpack:build', 'jekyll:build'])
+  gulp.watch(live.html).on('change', browserSync.reload)
+})
 
-gulp.task('build', ['img:build'], () => {
-	return gulp.src(live.all + '.{txt,xml,ico,png}')
-		.pipe(gulp.dest(build.dest));
-});
-
-//==========================================================//
-//=====================Gulp Serve===========================//
-//==========================================================//
-
-gulp.task('serve', ['jekyll', 'sass', 'gulpWebpack'], () => {
-
-    browserSync.init({
-        server: "./_live"
-    });
-
-    gulp.watch(source.sass, ['sass', 'jekyll']);
-    gulp.watch(source.html, ['jekyll']);
-    gulp.watch(source.js, ['gulpWebpack', 'jekyll']);
-    gulp.watch(live.html).on('change', browserSync.reload);
-});
-
-//==========================================================//
-//=====================Gulp deploy===========================//
-//==========================================================//
-
+//========== Run Deploy ========//
 gulp.task('deploy', ['build'], () => {
   return gulp.src('./build/**/*')
-    .pipe(ghPages());
-});
+    .pipe(ghPages())
+})
